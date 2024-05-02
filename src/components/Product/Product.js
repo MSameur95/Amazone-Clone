0f905ebe2d77f09d6f110b6f331e58aa1842425d
MSameur95 <sameur.mohammad@gmail.com>
2024-05-02 19:08:03 +0600

    amazone clone starting

---
+++ src/components/Product/Product.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+
+const Product = (props) => {
+    console.log(props.product);
+    const { name, img, seller, price, stock } = props.product;
+    return (
+        <div className='product'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+
+            <div>
+                <h5>{name}</h5>
+                <p><small>by:</small></p>
+                <p>${price}</p>
+                <p><small>only {stock} left in stock - order soon</small></p>
+                <button className="main-button" onClick={() =>props.handleAddProduct(props.product)}><FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+            </div>
+
+
+        </div>
+
+
+    );
+};
+
+export default Product;
